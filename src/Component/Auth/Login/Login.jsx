e91b6f2a65746792229edff4bbcc33357515acc9
youngmentor <olaleresuliton43@gmail.com>
2023-06-05 09:16:46 +0100

    new commit

--- src/Component/Auth/Login/Login.jsx
+++ src/Component/Auth/Login/Login.jsx
@@ -21,16 +21,16 @@ const Login = () => {
                             <p>Don't have an account here? login here </p>
                         </H1>
                         <Card>
-                            <H1 style={{ textAlign: "center" }}>Log in to your account</H1>
-                            <Span>Don't have an account? <Span2 to='/register'>Sign up</Span2></Span>
+                            {/* <H1 style={{ textAlign: "center" }}>Log in to your account</H1> */}
+                            {/* <Span>Don't have an account? <Span2 to='/register'>Sign up</Span2></Span> */}
                             <Form onSubmit={(e) => Login(e)}>
-                                <Label><MdEmail style={{ marginRight: "2%" }} />Username*</Label>
+                                {/* <Label><MdEmail style={{ marginRight: "2%" }} />Username*</Label> */}
                                 <Input type="text" placeholder=' Enter username' />
 
-                                <Label><BsFillLockFill style={{ marginRight: "2%" }} />Password*</Label>
+                                {/* <Label><BsFillLockFill style={{ marginRight: "2%" }} />Password*</Label> */}
                                 <Input type="password" placeholder='Enter password' />
-                                <Button>{loading ? <SpinnerCircular size={25} thickness={100} speed={100} color="rgba(255, 255, 255, 1)" secondaryColor="rgba(0, 0, 0, 0.44)" /> : "Log in"}</Button>
                                 <PassWordText to="/forgotpassword"><span style={{ color: "#FF4D4D", cursor: "pointer", textDecoration: "underline" }}>Forgot your password?</span></PassWordText>
+                                <Button>{loading ? <SpinnerCircular size={25} thickness={100} speed={100} color="rgba(255, 255, 255, 1)" secondaryColor="rgba(0, 0, 0, 0.44)" /> : "Log in"}</Button>
                             </Form>
                         </Card>
                     </RightWrap>
--- src/Component/Auth/Login/LoginStyle.jsx
+++ src/Component/Auth/Login/LoginStyle.jsx
@@ -17,7 +17,7 @@ export const Wrapper= styled.div`
 export const Right= styled.div`
     width: 40%;
     height: 100vh;
-    background-color: red;
+    background-color: white;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -46,23 +46,55 @@ export const Card= styled.div`
 export const Span= styled.div`
     
 `
-export const Input= styled.div`
-    
+export const Input= styled.input`
+   width :300px ;
+   height: 40px;
+   border-radius: 8px;
+   border: 1px solid lightgray;
+   background-color: transparent;
+   padding: 10px;
+   outline: none;
 `
 export const Span2= styled.div`
     
 `
 export const Label= styled.div`
-    
+     color: rgb(255, 255, 255);
+    font-size: 14px;
+    font-family: sans-serif, monospace;
+    font-weight: 400;
+    line-height: 14px;
+    margin-bottom: 1%;
+    display: flex;
+    align-items: center;
+ margin-right: 150px;
 `
 export const Button= styled.div`
-    
+     border: 0px;
+    display: inline-block;
+    background: #242424;
+    border-radius: 5px;
+    padding: 10px 20px;
+    color: #fff;
+    font-size: 14px;
+    margin-top: 5%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    width: 280px;
+    cursor: pointer;
 `
 export const PassWordText= styled.div`
     
 `
 export const Form= styled.div`
-    
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
 `
 export const SpinnerCircular= styled.div`
     
